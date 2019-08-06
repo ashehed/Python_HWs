4118a813cbe3517a04a1cdc92da3fa6f62b20ec6
ashehed <53475942+ashehed@users.noreply.github.com>
2019-08-06 12:44:26 +0300

    Adding HWs

---
+++ shehedin_homework1_0.py
@@ -0,0 +1,9 @@
+a = int(input("a = ")) 
+b = int(input("b = "))
+
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+
+print(addition, subtraction, multiplication, division)
